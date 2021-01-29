ed056dd9dd95d8d630e98f9eac0ccdd8131d8871
ebolart <fantabosco993@gmail.com>
2021-01-29 15:17:56 +0100

    added bootstrap css in index.html

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -6,6 +6,7 @@ import lombok.EqualsAndHashCode;
 import javax.persistence.*;
 import java.util.Set;
 
+//@EqualsAndHashCode(exclude = {"recipes"})
 @Data
 @EqualsAndHashCode(exclude = {"recipes"})
 @Entity
